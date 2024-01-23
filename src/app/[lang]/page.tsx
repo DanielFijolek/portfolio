0eab87d323c370cd0ffcb6b1b4f0b0d8f98fbb97
DanielFijolek <67265581+DanielFijolek@users.noreply.github.com>
2024-01-23 13:12:57 +0100

    Add glowing effect

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -12,7 +12,7 @@ export default async function Home({ params: { lang } }: Props) {
     <main className="flex min-h-screen flex-col items-center justify-center gap-2 p-24">
       <h1 className="text-3xl font-bold">Daniel Fijołek</h1>
       {dict.title}
-      <div className="mt-10 flex flex-col items-center gap-2 rounded-md border-2 border-blue-500 bg-slate-800 p-4">
+      <div className="drop-shadow-glow mt-10 flex flex-col items-center gap-2 rounded-md border-2 border-violet-600 bg-slate-900 p-4">
         <p>Portfolio website</p>
         <p className="text-lg">Work in progress</p>
       </div>
