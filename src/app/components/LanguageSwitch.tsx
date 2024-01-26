b18ec25d1453b504dfb5aa05a0fbd96f46209bfc
DanielFijolek <danielfijolek98@gmail.com>
2024-01-26 23:44:58 +0100

    Refactor LanguageSwitch component

--- src/app/components/LanguageSwitch.tsx
+++ src/app/components/LanguageSwitch.tsx
@@ -6,24 +6,21 @@ type Props = {
   currentLang: string;
 };
 
+const env = process.env.NODE_ENV;
+
 const LanguageSwitch = ({ currentLang }: Props) => {
   return (
-    <div className="group m-2 flex gap-1">
-      <div className="drop-shadow-glow bg-slate-900px-2 mx-2 flex w-10 flex-row justify-center rounded-md border-2 border-violet-800 py-1 group-hover:opacity-0">
-        {currentLang}
-      </div>
-      <div className="drop-shadow-glow absolute mx-2 hidden w-10 flex-col gap-2 divide-y-2 rounded-md border-2 border-violet-800 bg-slate-900 px-2 py-1 group-hover:flex">
-        {languages.map((lang) => (
-          <Link
-            className="flex flex-row justify-center"
-            href={`/${lang}`}
-            locale={lang}
-            key={lang}
-          >
-            {lang}
-          </Link>
-        ))}
-      </div>
+    <div className="flex w-10 flex-col divide-y-2 rounded-md border-2 border-violet-800 bg-slate-900 drop-shadow-glow">
+      {languages.map((lang) => (
+        <Link
+          className={`flex flex-row justify-center border-violet-800   py-1 ${currentLang === lang ? "text-secondary" : null}`}
+          href={`/${lang === "en" && env === "production" ? null : lang}`}
+          locale={lang}
+          key={lang}
+        >
+          {lang}
+        </Link>
+      ))}
     </div>
   );
 };
