080673148fc0c5a030c9402d89268ae1b269dc7f
DanielFijolek <67265581+DanielFijolek@users.noreply.github.com>
2024-01-27 10:46:27 +0100

    Add external link icon to project names

--- src/app/[lang]/ProjectsTab.tsx
+++ src/app/[lang]/ProjectsTab.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import TabContainer from "./TabContainer";
 import SkilltagContainer from "../components/SkilltagContainer";
 import { getDictionary, Locale } from "./dictionaries";
+import externalLink from "../../../public/images/external-link.svg";
+import Image from "next/image";
 
 type Props = {
   lang: Locale;
@@ -15,11 +17,20 @@ const ProjectsTab = async ({ lang }: Props) => {
       {dict.projects.map((element, idx) => (
         <a
           href={element.link}
-          className="flex flex-col gap-y-4 rounded-md p-4 transition-all duration-300 hover:bg-violet-800/20 hover:drop-shadow-glow focus:bg-violet-800/20 focus:outline-none focus:drop-shadow-glow"
+          className="group flex flex-col gap-y-4 rounded-md p-4 transition-all duration-300 hover:bg-violet-800/20 hover:drop-shadow-glow focus:bg-violet-800/20 focus:outline-none focus:drop-shadow-glow"
           key={idx}
         >
-          <div className="flex flex-row items-baseline gap-x-10">
+          <div className="flex flex-row gap-x-4 align-middle">
             <p className="text-lg">{element.name}</p>
+            {element.link ? (
+              <Image
+                className="group-hover:mb-2 group-hover:ml-2"
+                width={20}
+                height={20}
+                src={externalLink}
+                alt={`Link to project ${element.name}`}
+              />
+            ) : null}
           </div>
           <SkilltagContainer skills={element.tech} skillType="tech" />
           <div className="flex flex-col gap-2">
